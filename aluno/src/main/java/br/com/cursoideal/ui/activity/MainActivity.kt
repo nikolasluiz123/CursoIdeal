ffa66b47704822d513ddd9acb496baddfd3637d9
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2022-10-06 22:40:08 -0300

    feat: navigation drawer com os menus da aplicação
    Implementação apenas do menu de lougout, os demais não tem comportamento
    ainda.

--- aluno/src/main/java/br/com/cursoideal/ui/activity/MainActivity.kt
+++ aluno/src/main/java/br/com/cursoideal/ui/activity/MainActivity.kt
@@ -1,15 +1,20 @@
 package br.com.cursoideal.ui.activity
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import br.com.cursoideal.NavGraphDirections
 import br.com.cursoideal.R
 import br.com.cursoideal.databinding.ActivityMainBinding
 import br.com.cursoideal.ui.viewmodel.AppStateViewModel
+import br.com.cursoideal.ui.viewmodel.AuthenticationViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
@@ -17,9 +22,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
-    private val controller by lazy { findNavController(R.id.nav_host_fragment_content_main) }
-
+    private val navController by lazy { findNavController(R.id.nav_host_fragment_content_main) }
     private val appStateViewModel: AppStateViewModel by viewModel()
+    private val authenticationViewModel: AuthenticationViewModel by viewModel()
+
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,13 +34,30 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        toggle = ActionBarDrawerToggle(this@MainActivity, binding.drawerLayout, R.string.open, R.string.close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
         setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        appBarConfiguration = AppBarConfiguration(controller.graph)
-        setupActionBarWithNavController(controller, appBarConfiguration)
+        binding.navigationView.setNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.menu_item_user_profile -> onUserProfileSelected()
+                R.id.menu_item_courses_evaluation -> onCoursesEvaluationSelected()
+                R.id.menu_item_favorite_courses -> onFavoriteCoursesSelected()
+                R.id.menu_item_cursos -> onCursosSelected()
+                R.id.menu_item_cadastro_cursos -> onCadastroCursosSelected()
+                R.id.menu_item_logout -> onLogoutSelected()
+                R.id.menu_item_settings -> onSettingsSelected()
+                else -> true
+            }
+        }
 
-        controller.addOnDestinationChangedListener { _, destination, _ ->
+        appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
 
+        navController.addOnDestinationChangedListener { _, destination, _ ->
             appStateViewModel.components.observe(this) {
                 it?.let {  viewControll ->
                     if(viewControll.showAppBar) { supportActionBar?.show() } else { supportActionBar?.hide() }
@@ -42,6 +66,51 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+
+    private fun onUserProfileSelected(): Boolean {
+        return true
+    }
+
+    private fun onCoursesEvaluationSelected(): Boolean {
+        return true
+    }
+
+    private fun onFavoriteCoursesSelected(): Boolean {
+        return true
+    }
+
+    private fun onCursosSelected(): Boolean {
+        return true
+    }
+
+    private fun onCadastroCursosSelected(): Boolean {
+        return true
+    }
+
+    private fun onLogoutSelected(): Boolean {
+        binding.drawerLayout.close()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            authenticationViewModel.logout()
+            navController.navigate(NavGraphDirections.actionGlobalLoginFragment())
+        }, 200)
+
+        return true
+    }
+
+    private fun onSettingsSelected(): Boolean {
+        return true
+    }
+
+
     override fun onSupportNavigateUp(): Boolean =
-        controller.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+        navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
 }
--- aluno/src/main/java/br/com/cursoideal/ui/fragment/base/AbstractSessionedFragment.kt
+++ aluno/src/main/java/br/com/cursoideal/ui/fragment/base/AbstractSessionedFragment.kt
@@ -18,40 +18,9 @@ abstract class AbstractSessionedFragment : AbstractAuthenticableFragment() {
         verifyLoggedUser()
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        configureMenu()
-    }
-
-    private fun configureMenu() {
-        val menuHost: MenuHost = requireActivity()
-        menuHost.addMenuProvider(object : MenuProvider {
-            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-                menuInflater.inflate(R.menu.menu_logout, menu)
-            }
-
-            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-                if (menuItem.itemId == R.id.menu_item_logout) {
-                    onLogout()
-                }
-
-                return true
-            }
-        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
-    }
-
-    private fun onLogout() {
-        authenticationViewModel.logout()
-        navigateToLogin()
-    }
-
     private fun verifyLoggedUser() {
         if (authenticationViewModel.isNotLogged()) {
-            navigateToLogin()
+            navController.navigate(NavGraphDirections.actionGlobalLoginFragment())
         }
-    }
-
-    private fun navigateToLogin() {
-        navController.navigate(NavGraphDirections.actionGlobalLoginFragment())
     }
 }
